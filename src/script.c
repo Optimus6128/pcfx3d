70434f49c9ad9a6a1645b0142dc80a62c0bfda80
Michael Kargas <optimus6128@yahoo.gr>
2022-05-29 15:06:05 +0100

    Trying to init things in a way I can switch between two objects or more (so that I build less CDs to test the cube and columnoid at the same time). For some reason, deleting the param array points now crashes :(

--- src/script.c
+++ src/script.c
@@ -22,54 +22,84 @@ static int zoom=256;
 static const int rotVel = 2;
 static const int zoomVel = 2;
 
-static Mesh *softMesh8;
-static Mesh *softMesh16;
-
 static Texture *cloudTex8;
 static Texture *cloudTex16;
+static Object3D *softCubeObj, *softColumnoidObj;
 static Object3D *softObj;
 
 static int renderSoftMethodIndex = RENDER_SOFT_METHOD_GOURAUD;
 
 static bool autoRot = true;
 
+
+static Object3D *initMeshObject(int meshgenId, const MeshgenParams params, int optionsFlags, Texture *tex)
+{
+	Object3D *meshObj;
+
+	Mesh *softMesh = initGenMesh(meshgenId, params, optionsFlags, tex);
+	meshObj = initObject3D(softMesh);
+	setObject3Dmesh(meshObj, softMesh);
+
+	return meshObj;
+}
+
+static MeshgenParams initMeshObjectParams(int meshgenId)
+{
+	MeshgenParams params;
+
+	switch(meshgenId) {
+		case MESH_CUBE:
+		{
+			params = makeDefaultMeshgenParams(96);
+		}
+		break;
+
+		case MESH_SQUARE_COLUMNOID:
+		{
+			int i;
+			const int numPoints = 8;
+			const int size = 64;
+			Point2Darray *ptArray = initPoint2Darray(numPoints);
+
+			for (i=0; i<numPoints; ++i) {
+				const int y = (size/4) * (numPoints/2 - i);
+				const int r = ((SinF16((i*20) << 16) * (size / 2)) >> 16) + size / 2;
+				addPoint2D(ptArray, r,y);
+			}
+			params = makeMeshgenSquareColumnoidParams(size, ptArray->points, numPoints, true, true);
+
+			// destroyPoint2Darray(ptArray); //why it crashes now?
+		}
+		break;
+	}
+
+	return params;
+}
  
 static void effectMeshSoftInit(int bpp)
 {
-	int i;
-	const int numPoints = 8;
-	const int size = 96;
-	MeshgenParams params;
+	const int lightingOptions = MESH_OPTION_ENABLE_LIGHTING | MESH_OPTION_ENABLE_ENVMAP;
+	MeshgenParams paramsCube = initMeshObjectParams(MESH_CUBE);
+	MeshgenParams paramsColumnoid = initMeshObjectParams(MESH_SQUARE_COLUMNOID);
 
-	Point2Darray *ptArray = initPoint2Darray(numPoints);
-	
-	int meshType = MESH_CUBE; //MESH_SQUARE_COLUMNOID;
-
-	for (i=0; i<numPoints; ++i) {
-		const int y = (size/4) * (numPoints/2 - i);
-		const int r = ((SinF16((i*20) << 16) * (size / 2)) >> 16) + size / 2;
-		addPoint2D(ptArray, r,y);
-	}
-
-	params = makeMeshgenSquareColumnoidParams(size, ptArray->points, numPoints, true, true);
+	const int texWidth = 64;
+	const int texHeight = 64;
 
 	if (bpp==8) {
-		cloudTex8 = initGenTexture(64, 64, 8, NULL, 1, TEXGEN_CLOUDS, false, NULL);
-		softMesh8 = initGenMesh(meshType, params, MESH_OPTION_RENDER_SOFT8 | MESH_OPTION_ENABLE_LIGHTING | MESH_OPTION_ENABLE_ENVMAP, cloudTex8);
-
-		softObj = initObject3D(softMesh8);
-		setObject3Dmesh(softObj, softMesh8);
+		const int optionsFlags = MESH_OPTION_RENDER_SOFT8 | lightingOptions;
+		cloudTex8 = initGenTexture(texWidth, texHeight, 8, NULL, 1, TEXGEN_CLOUDS, false, NULL);
+		softCubeObj = initMeshObject(MESH_CUBE, paramsCube, optionsFlags, cloudTex8);
+		softColumnoidObj = initMeshObject(MESH_SQUARE_COLUMNOID, paramsColumnoid, optionsFlags, cloudTex8);
 	} else {
-		cloudTex16 = initGenTexture(64, 64, 16, NULL, 1, TEXGEN_CLOUDS, false, NULL);
-		softMesh16 = initGenMesh(meshType, params, MESH_OPTION_RENDER_SOFT16 | MESH_OPTION_ENABLE_LIGHTING | MESH_OPTION_ENABLE_ENVMAP, cloudTex16);
+		const int optionsFlags = MESH_OPTION_RENDER_SOFT16 | lightingOptions;
+		cloudTex16 = initGenTexture(texWidth, texHeight, 16, NULL, 1, TEXGEN_CLOUDS, false, NULL);
+		softCubeObj = initMeshObject(MESH_CUBE, paramsCube, optionsFlags, cloudTex16);
+		softColumnoidObj = initMeshObject(MESH_SQUARE_COLUMNOID, paramsColumnoid, optionsFlags, cloudTex16);
 
-		softObj = initObject3D(softMesh16);
-		setObject3Dmesh(softObj, softMesh16);
 	}
-
-	destroyPoint2Darray(ptArray);
-
 	setRenderSoftMethod(renderSoftMethodIndex);
+
+	softObj = softCubeObj;
 }
 
 static void inputScript()
